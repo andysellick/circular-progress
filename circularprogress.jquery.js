c1184f2acceb73ef93861f2ec6c4dd5580da3990
andysellick <andy.sellick@tangentsnowball.com>
2015-07-10 11:15:20 +0100

    Code restructuring

--- circularprogress.jquery.js
+++ circularprogress.jquery.js
@@ -10,14 +10,7 @@
 		this.elem = elem;
 		this.$elem = $(elem);
 		this.options = options;
-		/* syntax to use outside the plugin - thankyou http://www.virgentech.com/blog/2009/10/building-object-oriented-jquery-plugin.html
-            var moo = $('.newtest1').data('myplugin');
-            moo.publicMethod();
-		*/
-		//a public method oh my that took a lot of research
-		this.publicMethod = function(){
-            this.circles.circle.animateCircle(this.overallpos,300);
-        };
+		this.init(); //FIXME have to manually call the init function - not sure why
 	}
 
 	Plugin.prototype = {
@@ -37,127 +30,129 @@
 
 			this.rpanel; //right
 			this.lpanel; //left
-			this.timer;
 			this.overallpos = 0;
 			this.inner;
 
-            this.circles = {
-                general: {
-                    //create required variables
-                    initSettings: function(){
-                        //normalise settings
-                        me.settings.rotateBy = Math.min(me.settings.rotateBy,360);
-                        me.settings.initialPerc = Math.min(me.settings.initialPerc,100);
-                        me.settings.targetDeg = Math.min(me.settings.targetDeg,360);
+            //create required variables and normalise settings
+            this.settings.rotateBy = Math.min(this.settings.rotateBy,360);
+            this.settings.initialPerc = Math.min(this.settings.initialPerc,100);
+            this.settings.targetDeg = Math.min(this.settings.targetDeg,360);
+            this.rotateBy = this.settings.rotateBy; //fixme this is currently used but probably isn't needed
 
-                        me.rotateBy = me.settings.rotateBy;
-                    },
-                    //create required elements
-                    initElements: function(){
-                        var prog = $('<div/>').addClass('progressinner').appendTo(me.$elem);
-                        var lpane = $('<div/>').addClass('lpane').appendTo(prog);
-                        var rpane = $('<div/>').addClass('rpane').appendTo(prog);
-                        
-                        me.rpanel = $('<div/>').addClass('cover').appendTo(rpane);
-                        me.lpanel = $('<div/>').addClass('cover').appendTo(lpane);
-
-                        if(me.settings.innerHTML.length){
-                            me.inner = $('<div/>').addClass('display').html(me.settings.innerHTML).appendTo(prog);
-                        }
-                    }
-                },
-                circle: {
-                    //set the position of the circle, no animation
-                    setTargetPos: function(targ){
-                        me.overallpos = targ;
-                        me.circles.circle.renderCircle();
-                    },
-                    //given a starting point and an end point, animate the progress
-                    //self calls itself until complete
-                    animateCircle: function(orig,targ){
-                        var rotateby = me.settings.rotateBy;
-                        if(targ < orig){
-                            rotateby = -rotateby;
-                        }
-                        var newpos = orig + rotateby;
-                        if(orig < targ){
-                            newpos = Math.min(newpos,targ);
-                        }
-                        else {
-                            newpos = Math.max(newpos,targ);
-                        }
-                        me.overallpos = newpos;
-                        if(newpos != targ){
-                            setTimeout(function(){
-                                me.circles.circle.animateCircle(newpos,targ);
-                            },me.settings.speed);
-                        }
-                        me.circles.circle.renderCircle();
-                    },
-                    //draws the circular progress using the current position
-                    renderCircle: function(){
-                        if(me.overallpos < 180){
-                            me.circles.circle.rotateElement(me.rpanel,me.overallpos);
-                            me.circles.circle.rotateElement(me.lpanel,0);
-                        }
-                        else {
-                            me.circles.circle.rotateElement(me.rpanel,180);
-                            me.circles.circle.rotateElement(me.lpanel,me.overallpos - 180);
-                        }
-                    },
-                    //given an element, apply a css transform to rotate it
-                    rotateElement: function(elem,deg){
-                        elem.css({
-                            'transform': 'rotate('+deg+'deg)',
-                            '-ms-transform': 'rotate('+deg+'deg)',
-                            '-moz-tranform': 'rotate('+deg+'deg)',
-                            '-webkit-transform': 'rotate('+deg+'deg)',
-                            '-o-transform': 'rotate('+deg+'deg)'
-                        });
-                    }
-                }
-
-            };
-            me.circles.general.initSettings();
-            me.circles.general.initElements();
-
+            //create required elements
+            var prog = $('<div/>').addClass('progressinner').appendTo(this.$elem);
+            var lpane = $('<div/>').addClass('lpane').appendTo(prog);
+            var rpane = $('<div/>').addClass('rpane').appendTo(prog);
+            this.rpanel = $('<div/>').addClass('cover').appendTo(rpane);
+            this.lpanel = $('<div/>').addClass('cover').appendTo(lpane);
+            if(this.settings.innerHTML.length){
+                this.inner = $('<div/>').addClass('display').html(this.settings.innerHTML).appendTo(prog);
+            }
+            //now get the plugin started
             //option 1 - progress animates from initial to target
-            if(me.settings.initialDeg && me.settings.animateOnLoad){
-                if(me.settings.initialDeg > me.settings.targetDeg){ //if target is less than initial, we need to rotate backwards
-                    me.rotateBy = -me.rotateBy;
+            var me = this;
+
+            if(this.settings.initialDeg && this.settings.animateOnLoad){
+                if(this.settings.initialDeg > this.settings.targetDeg){ //if target is less than initial, we need to rotate backwards
+                    this.rotateBy = -this.rotateBy;
                 }
-                me.circles.circle.setTargetPos(me.settings.initialDeg);
+                this.setTargetPos(this.settings.initialDeg);
                 setTimeout(function(){
-                    me.circles.circle.animateCircle(me.settings.initialDeg,me.settings.targetDeg);
-                },me.settings.speed + me.settings.delayAnimation);
+                    me.animateCircle(me.settings.initialDeg,me.settings.targetDeg);
+                },this.settings.speed + this.settings.delayAnimation);
 
             }
             //option 2 - progress animates from 0 to target (no initial value)
-            else if(me.settings.animateOnLoad){
+            else if(this.settings.animateOnLoad){
                 setTimeout(function(){
-                    me.circles.circle.animateCircle(me.settings.initialDeg,me.settings.targetDeg);
-                },me.settings.speed + me.settings.delayAnimation);
+                    me.animateCircle(me.settings.initialDeg,me.settings.targetDeg);
+                },this.settings.speed + this.settings.delayAnimation);
             }
             //option 3 - progress appears immediately at target (no initial value, no animate)
             else {
-                me.circles.circle.setTargetPos(me.settings.initialDeg);
+                this.setTargetPos(this.settings.initialDeg);
             }
-		},
-		publicMethod: function(){
-            console.log('moo');
+        },
+        
+        //set the position of the circle, no animation
+        setTargetPos: function(targ){
+            this.overallpos = targ;
+            this.renderCircle();
+        },
+
+        //given a starting point and an end point, animate the progress
+        //self calls itself until complete
+        animateCircle: function(orig,targ){
+            var me = this;
+            var rotateby = this.settings.rotateBy;
+            if(targ < orig){
+                rotateby = -rotateby;
+            }
+            var newpos = orig + rotateby;
+            if(orig < targ){
+                newpos = Math.min(newpos,targ);
+            }
+            else {
+                newpos = Math.max(newpos,targ);
+            }
+            this.overallpos = newpos;
+            if(newpos != targ){
+                setTimeout(function(){
+                    me.animateCircle(newpos,targ);
+                },this.settings.speed);
+            }
+            this.renderCircle();
+        },
+
+        //draws the circular progress using the current position
+        renderCircle: function(){
+            if(this.overallpos < 180){
+                this.rotateElement(this.rpanel,this.overallpos);
+                this.rotateElement(this.lpanel,0);
+            }
+            else {
+                this.rotateElement(this.rpanel,180);
+                this.rotateElement(this.lpanel,this.overallpos - 180);
+            }
+        },
+
+        //given an element, apply a css transform to rotate it
+        rotateElement: function(elem,deg){
+            elem.css({
+                'transform': 'rotate('+deg+'deg)',
+                '-ms-transform': 'rotate('+deg+'deg)',
+                '-moz-tranform': 'rotate('+deg+'deg)',
+                '-webkit-transform': 'rotate('+deg+'deg)',
+                '-o-transform': 'rotate('+deg+'deg)'
+            });
+        },
+
+		moveProgress: function(targ){
+            this.animateCircle(this.overallpos,targ);
+        },
+
+        publicf: function(){
+            console.log('public');
+        },
+
+        _privateMethod: function(){
+            console.log('private');
         }
 	}
 	$.fn.circles = function(options){
-		return this.each(function(){
-			//new Plugin(this,options).init();
-            var el = $(this);
-            // Return early if this element already has a plugin instance
-            if (el.data('myplugin')) return;
-            var myplugin = new Plugin(this,options);
-            myplugin.init();
-            // Store plugin object in this element's data
-            el.data('myplugin', myplugin);
-		});
+		/* syntax to use outside the plugin - http://acuriousanimal.com/blog/2013/02/25/things-i-learned-creating-a-jquery-plugin-part-ii/
+            var circle = $('.element').data('circles');
+            circle.publicMethod();
+		*/
+        if (options === undefined || typeof options === 'object') {
+            //create plugin instance for each element and store reference to the plugin within the data attr
+            return this.each(function() {
+                if (!$.data(this, 'circles')) {
+                    var myplugin = new Plugin(this, options);
+                    $.data(this, 'circles', new Plugin(this, options));
+                }
+            });
+        }
 	}
 	window.Plugin = Plugin;
 })(window,jQuery);
